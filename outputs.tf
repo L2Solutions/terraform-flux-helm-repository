74ed1a6ffc0d62b85ae08821527eadb279ecfcf1
James <jamesedwardcooke@gmail.com>
2022-05-17 13:08:47 -0500

    Output name and manifest (#3)
    * Output name and manifest
    
    * terraform-docs: automated action
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,12 @@
+output "manifest" {
+  value = local.manifest
+}
+
+output "name" {
+  description = "Name of the repository resource with prefix(if enabled)"
+  value       = local.name
+}
+
 output "secret_name" {
   description = "Auth secret name"
   value       = local.secret_name
